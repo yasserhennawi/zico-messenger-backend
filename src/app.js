ef313e6f690e8c3acc823bbdea63fef33eafd959
yasserhennawi <yahennawi@gmail.com>
2020-07-06 05:50:46 +0200

    fix: update app.js

--- src/app.js
+++ src/app.js
@@ -1,24 +1,24 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import routes from './controllers';
-// required to enable environment variables by dotenv lib. 
-import { } from 'dotenv/config'
-import mongoose from './mongoose';
+import express from "express";
+import bodyParser from "body-parser";
+// required to enable environment variables by dotenv lib.
+import {} from "dotenv/config";
+import mongoose from "./mongoose";
+import appRouter from "./routes";
 
 // Opening database connection
 mongoose();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(bodyParser.json());
 
 app.listen(PORT, () => {
-    console.log("Server is running on port: " + PORT)
-})
+  console.log("Server is running on port: " + PORT);
+});
 
 app.get("/", (req, res) => {
-    res.send("Hello, express!")
-})
+  res.send("Welcome to ZICO messenger server!");
+});
 
-routes(app)
+app.use("/v1", appRouter);
